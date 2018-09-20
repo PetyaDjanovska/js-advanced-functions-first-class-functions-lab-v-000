41092a5ad6d7b6b186f985d859b7fc735cf3bd30
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-20 20:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ function createFareMultiplier(int){
   }
 }
 
-function fareDoubler(int){
-  return int * 2;
-}
-
-function fareTripler(int){
-  return int * 3;
-}
+const fareDoubler = cerateFareMultiplier(2);
+// function fareDoubler(int){
+//   return int * 2;
+// }
+//
+// function fareTripler(int){
+//   return int * 3;
+// }
 
 function fetchSpecifiedDrivers(arrayOfDrivers, func){
   return func(arrayOfDrivers);
